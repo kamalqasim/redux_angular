cc7da80a6b85a6481176ecddd3fc899545d4b7fe
kamal qasimov <qasimlikamal@gmail.com>
2018-12-25 18:21:02 +0400

    redux

---
+++ src/app/read/read.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+
+import { AppState } from './../aap.state';
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.scss']
+})
+export class ReadComponent implements OnInit {
+
+  tutorials: Observable<Tutorial[]>;
+
+  constructor(private store: Store<AppState>) {
+    this.tutorials = store.select('tutorial');
+   }
+
+  ngOnInit() {
+  }
+
+}
